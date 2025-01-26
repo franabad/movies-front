d2a9d467a1faf59d87280c7f0a959c5964f09138
franabad <fam1096@hotmail.com>
2025-01-26 05:31:04 +0100

    Add children and logic to button component

--- src/app/(routes)/movies/components/AnimatedButtonComponent.tsx
+++ src/app/(routes)/movies/components/AnimatedButtonComponent.tsx
@@ -1,21 +1,28 @@
 import React, { useContext } from 'react'
 import { ChevronRight } from 'lucide-react'
 import { TicketsContext } from '@/context/tickets'
+import { SeatContext } from '@/context/selectedSeats'
 
 interface AnimatedButtonProps {
-  onClick: () => void
+  onClick: () => void,
+  children: React.ReactNode
 }
 
-const AnimatedButtonComponent = ({ onClick }: AnimatedButtonProps) => {
+const AnimatedButtonComponent = ({ onClick, children }: AnimatedButtonProps) => {
   const { tickets } = useContext(TicketsContext)
+  const { selectedSeats } = useContext(SeatContext)
 
   return (
     <button
-      className={`group gap-x-2 flex flex-row items-center justify-center rounded-xl bg-[#11117b] px-6 py-3 overflow-hidden transition-all duration-300 ease-out hover:bg-[#1a1a8f] hover:shadow-lg ${Object.values(tickets).every((value) => value === 0) ? 'pointer-events-none opacity-50' : ''}`}
+      className={
+        `group gap-x-2 flex flex-row items-center justify-center rounded-xl bg-[#11117b] px-6 py-3 overflow-hidden transition-all duration-300 ease-out hover:bg-[#1a1a8f] hover:shadow-lg 
+        ${Object.values(tickets).every((value) => value === 0) ? 'pointer-events-none opacity-50' : ''}
+        ${selectedSeats.length === 0 && children === 'Confirm' ? 'pointer-events-none opacity-50' : ''}
+      `}
       onClick={onClick}
     >
       <span>
-        Continuar
+        {children}
       </span>
       <ChevronRight
         size={22}
