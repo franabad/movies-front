668df2da7ddbe7562b755d8f0b59dbe800ea8d0c
franabad <fam1096@hotmail.com>
2025-01-28 04:05:15 +0100

    Change style, add new prop and delete unused Close tag and its import

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-import { X } from 'lucide-react'
 
 import '@/app/globals.css'
 
@@ -23,7 +22,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'fixed inset-0 z-50 bg-black/80 backdrop-blur-[1px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-20 bg-black/80 backdrop-blur-[1px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
@@ -33,22 +32,24 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 interface DialogContentProps
   extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> {
-  customCloseAnimation?: boolean | string,
-  animation?: string, // Nueva propiedad para usar animaciones personalizadas
-  center?: boolean
+  customAnimation?: boolean | string,
+  animation?: string,
+  center?: boolean,
+  aside?: boolean
 }
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   DialogContentProps
->(({ className, children, customCloseAnimation, animation, center, ...props }, ref) => (
+>(({ className, children, customAnimation, animation, center, aside, ...props }, ref) => (
   <DialogPortal>
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[35%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-4 shadow-lg sm:rounded-lg',
+        !aside && 'fixed left-[35%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-4 shadow-lg sm:rounded-lg',
+        aside && 'fixed right-0 top-0 z-50 bg-background shadow-lg sm:rounded-lg gap-4 border-l p-4 h-full w-1/6',
         center && 'left-[50%]',
-        customCloseAnimation
+        customAnimation
           ? animation // Usa la animación personalizada si está habilitada
           : 'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
         className
@@ -56,10 +57,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="h-4 w-4 hidden" />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
