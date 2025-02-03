da7f3782bc328bcba611f7e9ae64cffde3fcec01
franabad <fam1096@hotmail.com>
2025-02-03 06:14:58 +0100

    Add default login page

---
+++ src/app/(routes)/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="font-opensans items-center justify-items-center max-h-screen flex flex-col">
+      <p>Login Page</p>
+    </div>
+  )
+}
