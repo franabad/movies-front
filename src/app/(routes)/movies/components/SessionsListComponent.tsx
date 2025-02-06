0c1fe50270f113e52ef97201753631673a5ca724
franabad <fam1096@hotmail.com>
2025-02-06 03:53:26 +0100

    Refactor and add filter movies feature

--- src/app/(routes)/movies/components/SessionsListComponent.tsx
+++ src/app/(routes)/movies/components/SessionsListComponent.tsx
@@ -1,29 +1,69 @@
+/* eslint-disable indent */
 import { Session } from '@/app/types/session'
 import MovieRowComponent from './MovieRowComponent'
 import { Movie } from '@/app/types/movie'
+import { useEffect, useState } from 'react'
+import { Temporal } from 'temporal-polyfill'
 
 interface SessionsListComponentProps {
-  movies: Movie[]
+  selectedDate: string
 }
 
-export default function SessionsListComponent({ movies }: SessionsListComponentProps) {
-  const fetchSessions = async (movie: Movie) => {
-    const data = await fetch(`http://localhost:3000/api/sessions/movie/${movie.id}`)
-    const sessions: Session[] = await data.json()
+export default function SessionsListComponent({ selectedDate }: SessionsListComponentProps) {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [sessions, setSessionsByMovie] = useState<Session[]>([])
+  const [isLoading, setIsLoading] = useState(true)
 
-    return sessions
-  }
+  selectedDate = (selectedDate || Temporal.Now.plainDateISO().toString()).split('-').join('')
 
-  const lastMovieId = movies[movies.length - 1].id
+  useEffect(() => {
+    const fetchMoviesAndSessions = async () => {
+      setIsLoading(true)
+      try {
+        const moviesData = await fetch('http://localhost:3000/api/movies')
+        const movies = await moviesData.json()
+
+        setMovies(movies)
+
+        const sessionsData = await fetch(`http://localhost:3000/api/sessions?date=${selectedDate}`)
+        const sessions: Session[] = await sessionsData.json()
+
+        if (sessionsData.status === 404) {
+          setSessionsByMovie([])
+        } else {
+          setSessionsByMovie(sessions)
+        }
+      } catch (error) {
+        console.error('Error fetching movies and sessions: ', error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchMoviesAndSessions()
+  }, [selectedDate])
+
+  const lastMovieId = movies.length > 0 ? movies[movies.length - 1].id : null
+
+  console.log(sessions)
 
   return (
     <section className="items-center justify-center flex flex-col gap-y-5 w-full mt-8">
-      {movies.map(async (movie: Movie) => {
-        const sessions = await fetchSessions(movie)
-        return (
-          <MovieRowComponent key={movie.id} movie={movie} sessions={sessions} lastMovieId={lastMovieId} />
+      {isLoading
+        ? (
+          <p></p>
         )
-      })}
+        : sessions && sessions.length > 0
+          ? (
+            movies
+              .filter((movie: Movie) => sessions.some((session: Session) => session.movieId === movie.id))
+              .map((movie: Movie) => (
+                <MovieRowComponent key={movie.id} movie={movie} sessions={sessions.filter((session: Session) => session.movieId === movie.id)} lastMovieId={lastMovieId} />
+              ))
+          )
+          : (
+            <p>No movies available for this date</p>
+          )}
     </section>
   )
 }
