4ab348295910331bcc409782e3cedfb91ede1192
franabad <fam1096@hotmail.com>
2025-02-09 20:41:23 +0100

    Refactor to change dates to a specific format

--- src/app/(routes)/movies/components/DateDropdownComponent.tsx
+++ src/app/(routes)/movies/components/DateDropdownComponent.tsx
@@ -9,7 +9,7 @@ interface DateDropdownProps {
 
 const DateDropdownComponent = ({ setSelectedDate }: DateDropdownProps) => {
   const [isOpen, setIsOpen] = useState(false)
-  const [date, setDate] = useState('')
+  const [date, setDate] = useState(Temporal.Now.plainDateISO().toLocaleString('en-US', { weekday: 'short', day: '2-digit', month: 'short' }))
   const dropdownRef = useRef<HTMLDivElement>(null)
 
   const generateDates = () => {
@@ -19,7 +19,7 @@ const DateDropdownComponent = ({ setSelectedDate }: DateDropdownProps) => {
 
     for (let i = 0; i <= 7; i++) {
       const date = today.add({ days: i })
-      dates.push(date.toString())
+      dates.push(date)
     }
     return dates
   }
@@ -28,9 +28,9 @@ const DateDropdownComponent = ({ setSelectedDate }: DateDropdownProps) => {
 
   const toggleDropdown = () => setIsOpen(!isOpen)
 
-  const handleOptionClick = (date: string) => {
-    setDate(date)
-    setSelectedDate(date)
+  const handleOptionClick = (date: Temporal.PlainDate) => {
+    setDate(date.toLocaleString('en-US', { weekday: 'short', day: '2-digit', month: 'short' }))
+    setSelectedDate(date.toString())
     setIsOpen(!isOpen)
   }
 
@@ -45,25 +45,28 @@ const DateDropdownComponent = ({ setSelectedDate }: DateDropdownProps) => {
   }, [])
 
   return (
-    <div className="relative w-32" ref={dropdownRef}>
+    <div className="relative w-48" ref={dropdownRef}>
       <label htmlFor="dateDropdown" className="block mb-2 text-sm font-medium">Select a date: </label>
       <div
         onClick={toggleDropdown}
         className="cursor-pointer bg-gray-700/50 w-full rounded-md px-2 py-1 shadow-md"
       >
-        {date === Temporal.Now.plainDateISO().toString() ? 'Today' : date || 'Today'}
+        {date === Temporal.Now.plainDateISO().toLocaleString('en-US', { weekday: 'short', day: '2-digit', month: 'short' }) ? 'Today' : date}
       </div>
       {isOpen && (
         <ul className="cursor-pointer absolute w-full mt-1 bg-black border-[1px] border-gray-300/30 px-1 flex flex-col rounded-md shadow-lg">
-          {dates.map((date, index) => (
-            <li
-              key={date}
-              onClick={() => handleOptionClick(date)}
-              className={` border-gray-300/30 hover:bg-gray-200/20 px-2 py-1 rounded-md mt-1 transition-all duration-200 ease-in-out ${index === dates.length - 1 && 'mb-1'}`}
-            >
-              {date === Temporal.Now.plainDateISO().toString() ? 'Today' : date}
-            </li>
-          ))}
+          {dates.map((date, index) => {
+            const formattedDate = date.toLocaleString('en-US', { weekday: 'short', day: '2-digit', month: 'short' })
+            return (
+              <li
+                key={date.toString()}
+                onClick={() => handleOptionClick(date)}
+                className={` border-gray-300/30 hover:bg-gray-200/20 px-2 py-1 rounded-md mt-1 transition-all duration-200 ease-in-out ${index === dates.length - 1 && 'mb-1'}`}
+              >
+                {formattedDate === Temporal.Now.plainDateISO().toLocaleString('en-US', { weekday: 'short', day: '2-digit', month: 'short' }) ? 'Today' : formattedDate}
+              </li>
+            )
+          })}
         </ul>
       )}
     </div>
